9e321e100eecd0d472e67bcc26a024da57c63184
thefifthagreement <mickael.vitry@gmail.com>
2020-08-21 12:22:59 +0200

    avancement
    modif preprocess nom des sous-dossiers split
    script python train

--- open-unmix/train.py
+++ open-unmix/train.py
@@ -11,7 +11,7 @@ target_instrument = "clean_electric_guitar"
 target_file = "clean_electric_guitar.wav"
 model = wd_path.joinpath("output", "19-08-2020_1")
 dataset_type = "trackfolder_var"
-root = wd_path.joinpath("data")
+root = Path("C:\\umx\\data")
 output = wd_path.joinpath("output", "19-08-2020_1")
 epochs = "5"
 batch_size = "16"
@@ -37,5 +37,5 @@ if __name__ == "__main__":
     os.chdir(umx_path)
     start = time()
     #subprocess.call(args)
-    print(args)
+    print(*args)
     print("\nThe training lasted {:.2f} s".format(time()-start))
